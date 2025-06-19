eca57b13888aea2612eb8e94959b2ff3f3bd0c6c
Bhupi1306 <bhupesh0613@gmail.com>
2025-06-19 12:33:08 +0530

    refactor: Simplify routing structure by consolidating login and registration routes under the root path

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -9,40 +9,45 @@ import EntryForm from '../components/EntryForm.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/register" 
-        element={
-          <Form 
-            fullName={true}
-            backendUrl={`${import.meta.env.VITE_API_BASE_URL}/api/v1/auth/register`}
-            type="Registeration"
-            headerText="Create an Account"
-          />
-        } />
-      <Route path="/login" 
-        element={
-          <Form 
-            fullName={false}
-            backendUrl={`${import.meta.env.VITE_API_BASE_URL}/api/v1/auth/login`}
-            type="Login"
-            headerText="Login"
-          />
-        } />
 
-        <Route path = "/" 
+      <Route path='/'>
+
+        <Route index element={
+            <Form 
+              fullName={false}
+              backendUrl={`${import.meta.env.VITE_API_BASE_URL}/api/v1/auth/login`}
+              type="Login"
+              headerText="Login"
+            />
+          } />
+          
+        <Route path="login" 
           element={
             <Form 
-            fullName={false}
-            backendUrl={`${import.meta.env.VITE_API_BASE_URL}/api/v1/auth/login`}
-            type="Login"
-            headerText="Login"
-          />
-          }
-        />
+              fullName={false}
+              backendUrl={`${import.meta.env.VITE_API_BASE_URL}/api/v1/auth/login`}
+              type="Login"
+              headerText="Login"
+            />
+          } />
 
-      <Route path="/home" 
-        element={
-          <EntryForm />
-        } />
+
+        <Route path="register" 
+          element={
+            <Form 
+              fullName={true}
+              backendUrl={`${import.meta.env.VITE_API_BASE_URL}/api/v1/auth/register`}
+              type="Registeration"
+              headerText="Create an Account"
+            />
+          } />
+
+
+        <Route path="home" 
+          element={
+            <EntryForm />
+          } />
+      </Route>
     </Routes>
   </BrowserRouter>,
 )
